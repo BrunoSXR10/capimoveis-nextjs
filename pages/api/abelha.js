503662c9640733c4b500b7d8c1a7187aea80ac41
Bruno <82131199+BrunoSXR10@users.noreply.github.com>
2022-09-26 00:04:06 -0300

    TilwindCSS

---
+++ pages/api/abelha.js
@@ -0,0 +1,11 @@
+import announcement from "../announce";
+
+<announcement/>
+
+export default function buscaEndereco(){
+
+    let cap = document.getElementById('about').value
+    alert(cap)
+
+}
+
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Link from "../components/link";
+
+const Home = () => (
+  <div>
+
+      <h1 className="" >CAPIMÓVEIS</h1>
+      
+      <Link className='bg-red-500 text-white rounded px-3 py-1 font-bold text-center' href = '/announce'>
+          anuncie
+      </Link>
+  </div>
+)
+
+export default Home
+
