88f68938a5cafd31d927daae4d356a9eb88baa31
Bruno <82131199+BrunoSXR10@users.noreply.github.com>
2022-10-08 13:04:31 -0300

    useState

---
+++ pages/Login.jsx
@@ -0,0 +1,12 @@
+export default function Login () {
+    
+    
+    
+    return(
+        <div className= " bg-gradient-to-r from-purple-400 to-blue-600">
+            
+
+
+        </div>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Link from "../components/link";
+import Navbar from "../components/navbar";
+
+
+export default function Example() {
+  return (
+    <>
+    <Navbar/>
+     <a href="./api/ovo" className='flex justify-center'>hahaha</a> 
+    
+    </>
+  )
+}
